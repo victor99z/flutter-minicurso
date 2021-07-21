b90f99fc9e793937b620c990f613b44589034257
Victor Bernardes <victor_luizbernardes5@outlook.com>
2021-07-21 18:01:02 -0300

    saving

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_flutter/view/cliente_view.dart';
+
+void main() => runApp(const Robinho());
---
+++ lib/view/cliente_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Robinho extends StatelessWidget {
+  const Robinho({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Floating Action Button Label'),
+      ),
+      body: const Center(
+        child: Text('Press the button with a label below!'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        label: const Text('Approve'),
+        icon: const Icon(Icons.thumb_up),
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
+}
